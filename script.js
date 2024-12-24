b22d8b9a38c2da1ba8b3e5353f9b04ca7f213d1e
MShahrukh-786 <mshahrukh0636@gmail.com>
2024-12-25 00:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //your JS code here. If required.
+
+CSS
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+⌄
+⌄
+⌄
+/*  your css code here. If applicable */
+.book-library {
+background-color: #ffffff;
+padding: 20px;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+}
+
+.book {
+margin: 20px;
+width: 200px;
+text-align: center;
+}
+
+.price-block {
+background-color: #2ecc71;
+padding: 5px;
+border-radius: 5px;
+color: #ffffff;
+}
+
+Javascript
+
+1
+2
